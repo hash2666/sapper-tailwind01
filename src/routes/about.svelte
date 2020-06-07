96031a4c7f90b81d3e33712b5ff4e4be4bd152d9
hash2666 <huuut2544@gmail.com>
2020-06-07 23:02:52 +0900

    start

---
+++ src/routes/about.svelte
@@ -0,0 +1,26 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<div class="md:flex mt-20">
+  <div class="md:flex-shrink-0">
+    <img class="rounded-lg md:w-56" src="img03.png" alt="Woman paying for a purchase">
+  </div>
+  <div class="mt-4 md:mt-0 md:ml-6">
+    <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold mt-6">Marketing</div>
+    <a href="https://twitter.com/home?lang=ja" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+    <p class="mt-2 text-gray-600">Have fun with Sapper!</p>
+  </div>
+</div>
+<div class="md:flex mt-20">
+  <div class="md:flex-shrink-0">
+    <img class="rounded-lg md:w-56" src="img01.png" alt="Woman paying for a purchase">
+  </div>
+  <div class="mt-4 md:mt-0 md:ml-6">
+    <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold mt-6">Marketing</div>
+    <a href="https://twitter.com/home?lang=ja" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+    <p class="mt-2 text-gray-600">Have fun with Sapper!</p>
+  </div>
+</div>
---
+++ src/routes/index.svelte
@@ -0,0 +1,45 @@
+<style>
+	figure, p {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+
+</style>
+
+<svelte:head>
+	<title>Sapper project template</title>
+</svelte:head>
+
+
+
+<figure>
+
+	<div class="md:flex mt-16">
+  <div class="md:flex-shrink-0">
+    <img class="rounded-lg md:w-56" src="img04.png" alt="Woman paying for a purchase">
+  </div>
+  <div class="mt-4 md:mt-0 md:ml-6">
+    <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold mt-6">Making</div>
+    <a href="https://twitter.com/home?lang=ja" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">SapperBlog >> TailWindcss</a>
+    <p class="mt-2 text-gray-600">テストブログ！！</p>
+  </div>
+</div>
+</figure>
+
+<p><strong class="text-yellow-500">Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
