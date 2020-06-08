5f87e584dfd5b8198349d73c03cfaad8228c68cb
hash2666 <huuut2544@gmail.com>
2020-06-08 21:23:32 +0900

    gsap puls

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,61 +1,93 @@
 <script>
 	export let segment;
+	import { animate } from "./../animate.js";
+  let desc = false;
 </script>
 
+
+
+
+
 <style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
+main {
+    text-align: center;
+    padding: 1em;
+		font-size: 3.5em;
+    margin: 0 auto;
+		
+  }
+  nav {
+    border-bottom: 1px solid rgba(255, 62, 0, 0.1);
+    font-weight: 300;
+    padding: 0 1em;
+  }
 
-	ul {
-		margin: 0;
-		padding: 0;
-	}
+  ul {
+    margin: 0;
+    padding: 0;
+  }
 
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
+  /* clearfix */
+  ul::after {
+    content: "";
+    display: block;
+    clear: both;
+  }
 
-	li {
-		display: block;
-		float: left;
-	}
+  li {
+    display: block;
+    float: left;
+  }
 
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
+  [aria-current] {
+    position: relative;
+    display: inline-block;
+  }
 
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(87, 218, 196);
-		display: block;
-		bottom: -1px;
-	}
+  [aria-current]::after {
+    position: absolute;
+    content: "";
+    width: calc(100% - 1em);
+    height: 2px;
+    background-color: rgb(87, 218, 196);
+    display: block;
+    bottom: -1px;
+  }
 
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		font-size: 2em;
-		display: block;
-	}
+  a {
+    text-decoration: none;
+    padding: 1em 0.5em;
+    font-size: 2em;
+    display: block;
+  }
 </style>
 
 <nav>
-	<ul class="flex justify-center">
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
+  <ul class="flex justify-center">
+    <li>
+      <a aria-current={segment === undefined ? 'page' : undefined} href=".">
+        home
+      </a>
+    </li>
+    <li>
+      <a aria-current={segment === 'about' ? 'page' : undefined} href="about">
+        about
+      </a>
+    </li>
 
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+    <li>
+      <a
+        rel="prefetch"
+        aria-current={segment === 'blog' ? 'page' : undefined}
+        href="blog">
+        blog
+      </a>
+    </li>
+  </ul>
 </nav>
+<main 
+  use:animate={{ type: 'from', duration: 1,y:100, ease: "bounce",opacity: 0,scale:0.5, onComplete: () => (desc = true) }}>
+  <p class="text-secondary-600">Sapper >> TailWind >> Gsap !!!!!</p>
+</main>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,14 +30,15 @@
 
 <figure>
 
-	<div class="md:flex mt-16">
+	<div class="md:flex mt-12">
   <div class="md:flex-shrink-0">
     <img class="rounded-lg md:w-56" src="img04.png" alt="Woman paying for a purchase">
   </div>
   <div class="mt-4 md:mt-0 md:ml-6">
-    <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold mt-6">Making</div>
-    <a href="https://twitter.com/home?lang=ja" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">SapperBlog >> TailWindcss</a>
+    <div class="uppercase tracking-wide text-2xl text-indigo-600 font-bold mt-6">Making</div>
+    <!-- <a href="https://twitter.com/home?lang=ja" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">SapperBlog >> TailWindcss</a> -->
     <p class="mt-2 text-gray-600">テストブログ！！</p>
+		<p class="mt-2 text-gray-600">Ullam iusto magnam perferendis distinctio impedit non praesentium nemo perspiciatis temporibus consequatur necessitatibus, quam id. </p>
   </div>
 </div>
 </figure>
